693ba42df49d4e38bb6a23273f6a18a89badc60e
Ephrem-Adugna <ephremadugna1219@gmail.com>
2022-05-29 14:49:34 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
