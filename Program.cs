2a0d7d4b9aa42a385e22c65ea7eb5f0ea2049b2e
Ephrem-Adugna <ephremadugna1219@gmail.com>
2022-05-29 14:57:39 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace streamanalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
